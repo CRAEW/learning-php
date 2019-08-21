b10542425d2b1cab8f8f6d8af0a013754ca88744
Wendy Van Craen <wendy.vancraen@gmail.com>
2019-08-21 15:36:33 +0200

    card deck

--- exercises/4.classes/blackjack/blackjack.php
+++ exercises/4.classes/blackjack/blackjack.php
@@ -1,9 +1,40 @@
 <?php 
-class Cards {
 
+$deck = [];
 
+$cardvalues = array(
+    "A" => 11,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "10" => 10,
+    "J" => 10,
+    "Q" => 10,
+    "K" => 10
+);
 
+$suits = array("club", "diamond", "heart", "spade");
+
+foreach($suits as $suit) {
+    $cardnames = array_keys($cardvalues);
+    foreach($cardvalues as $cardname => $cardvalue) {
+        $card = $suit . " " . $cardname;
+        $deck[$card] = $cardvalue;
+    }
 }
+
+echo $deck["club 7"];
+
+// class Blackjack {
+
+
+
+// }
 
 
 
